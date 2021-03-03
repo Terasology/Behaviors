6cf43407811e1094c5e944d621e6f9ee4c4db660
DarkWeird <foxflameless@gmail.com>
2021-03-03 14:42:26 +0300

    chore(tests): remove unused code from `TestActionsMTE` and added asserts

--- src/test/java/org/terasology/minion/trees/TestActionsMTE.java
+++ src/test/java/org/terasology/minion/trees/TestActionsMTE.java
@@ -4,24 +4,24 @@
 package org.terasology.minion.trees;
 
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.terasology.assets.ResourceUrn;
 import org.terasology.assets.management.AssetManager;
-import org.terasology.context.Context;
 import org.terasology.entitySystem.entity.EntityManager;
-import org.terasology.logic.behavior.BehaviorSystem;
 import org.terasology.logic.behavior.asset.BehaviorTree;
 import org.terasology.logic.behavior.core.BehaviorTreeBuilder;
 import org.terasology.logic.behavior.nui.BehaviorEditor;
+import org.terasology.logic.behavior.nui.BehaviorNodeComponent;
 import org.terasology.logic.behavior.nui.BehaviorNodeFactory;
+import org.terasology.logic.behavior.nui.RenderableNode;
 import org.terasology.moduletestingenvironment.MTEExtension;
 import org.terasology.moduletestingenvironment.ModuleTestingHelper;
 import org.terasology.moduletestingenvironment.extension.Dependencies;
-import org.terasology.nui.properties.OneOfProviderFactory;
 import org.terasology.registry.In;
-import org.terasology.registry.InjectionHelper;
+
+import java.util.Optional;
 
 @ExtendWith(MTEExtension.class)
 @Dependencies({"engine", "ModuleTestingEnvironment", "Behaviors"})
@@ -33,31 +33,25 @@ class TestActionsMTE {
     private EntityManager entityManager;
     @In
     private AssetManager assetManager;
-
+    @In
     private BehaviorNodeFactory nodeFactory;
-
-    @BeforeEach
-    void setup() {
-        nodeFactory = new BehaviorNodeFactory();
-        BehaviorSystem behaviorSystem = new BehaviorSystem();
-        Context hostContext = helper.getHostContext();
-
-        hostContext.put(OneOfProviderFactory.class, new OneOfProviderFactory());
-        hostContext.put(BehaviorNodeFactory.class, nodeFactory);
-        InjectionHelper.inject(nodeFactory, hostContext);
-        hostContext.put(BehaviorSystem.class, behaviorSystem);
-        InjectionHelper.inject(behaviorSystem, hostContext);
-        nodeFactory.refreshLibrary();
-    }
 
     @Test
     void moveTo() {
-
         BehaviorTreeBuilder builder = new BehaviorTreeBuilder();
         BehaviorEditor editor = new BehaviorEditor();
         editor.initialize(helper.getHostContext());
 
-        editor.setTree(assetManager.getAsset(new ResourceUrn("Behaviors", "worker"), BehaviorTree.class).get());
-        editor.createNode(nodeFactory.getNodeComponent(builder.fromJson("move_to")));
+        ResourceUrn assetUrn = new ResourceUrn("Behaviors", "worker");
+        Optional<BehaviorTree> asset = assetManager.getAsset(assetUrn, BehaviorTree.class);
+        Assertions.assertTrue(asset.isPresent(), "Should present: " + assetUrn);
+
+        editor.setTree(asset.get());
+
+        BehaviorNodeComponent nodeComponent = nodeFactory.getNodeComponent(builder.fromJson("move_to"));
+        Assertions.assertNotEquals(BehaviorNodeComponent.DEFAULT, nodeComponent, "NodeComponent should present");
+
+        RenderableNode renderableNode = editor.createNode(nodeComponent);
+        Assertions.assertNotNull(renderableNode, "RenderableNode should be present");
     }
 }
