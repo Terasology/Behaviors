5eb67a9471f9a799c69abddcfa371e7a4828f889
DarkWeird <foxflameless@gmail.com>
2021-03-03 14:35:07 +0300

    chore(tests): remove old `TestActions` test and migrate TestActionsMTE to `MTEExtension`

--- src/test/java/org/terasology/minion/trees/TestActionsMTE.java
+++ src/test/java/org/terasology/minion/trees/TestActionsMTE.java
@@ -4,58 +4,60 @@
 package org.terasology.minion.trees;
 
 
-import com.google.common.collect.Sets;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.terasology.assets.ResourceUrn;
 import org.terasology.assets.management.AssetManager;
+import org.terasology.context.Context;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.logic.behavior.BehaviorSystem;
 import org.terasology.logic.behavior.asset.BehaviorTree;
 import org.terasology.logic.behavior.core.BehaviorTreeBuilder;
 import org.terasology.logic.behavior.nui.BehaviorEditor;
 import org.terasology.logic.behavior.nui.BehaviorNodeFactory;
-import org.terasology.moduletestingenvironment.ModuleTestingEnvironment;
+import org.terasology.moduletestingenvironment.MTEExtension;
+import org.terasology.moduletestingenvironment.ModuleTestingHelper;
+import org.terasology.moduletestingenvironment.extension.Dependencies;
 import org.terasology.nui.properties.OneOfProviderFactory;
+import org.terasology.registry.In;
 import org.terasology.registry.InjectionHelper;
 
-import java.util.Set;
+@ExtendWith(MTEExtension.class)
+@Dependencies({"engine", "ModuleTestingEnvironment", "Behaviors"})
+class TestActionsMTE {
 
-class TestActionsMTE extends ModuleTestingEnvironment {
+    @In
+    private ModuleTestingHelper helper;
+    @In
+    private EntityManager entityManager;
+    @In
+    private AssetManager assetManager;
 
-    protected EntityManager entityManager;
-
-    public Set<String> getDependencies() {
-        return Sets.newHashSet("engine", "ModuleTestingEnvironment", "Behaviors");
-    }
+    private BehaviorNodeFactory nodeFactory;
 
     @BeforeEach
-    void beforeMyModuleTests() {
-        entityManager = getHostContext().get(EntityManager.class);
-        runUntil(1000, () -> {
-            return true;
-        });
-    }
+    void setup() {
+        nodeFactory = new BehaviorNodeFactory();
+        BehaviorSystem behaviorSystem = new BehaviorSystem();
+        Context hostContext = helper.getHostContext();
 
+        hostContext.put(OneOfProviderFactory.class, new OneOfProviderFactory());
+        hostContext.put(BehaviorNodeFactory.class, nodeFactory);
+        InjectionHelper.inject(nodeFactory, hostContext);
+        hostContext.put(BehaviorSystem.class, behaviorSystem);
+        InjectionHelper.inject(behaviorSystem, hostContext);
+        nodeFactory.refreshLibrary();
+    }
 
     @Test
     void moveTo() {
 
-        BehaviorNodeFactory nodeFactory = new BehaviorNodeFactory();
-        BehaviorSystem behaviorSystem = new BehaviorSystem();
-
-        getHostContext().put(OneOfProviderFactory.class, new OneOfProviderFactory());
-        getHostContext().put(BehaviorNodeFactory.class, nodeFactory);
-        InjectionHelper.inject(nodeFactory, getHostContext());
-        getHostContext().put(BehaviorSystem.class, behaviorSystem);
-        InjectionHelper.inject(behaviorSystem, getHostContext());
-        nodeFactory.refreshLibrary();
-
         BehaviorTreeBuilder builder = new BehaviorTreeBuilder();
         BehaviorEditor editor = new BehaviorEditor();
-        editor.initialize(getHostContext());
+        editor.initialize(helper.getHostContext());
 
-        editor.setTree(getHostContext().get(AssetManager.class).getAsset(new ResourceUrn("Behaviors", "worker"), BehaviorTree.class).get());
+        editor.setTree(assetManager.getAsset(new ResourceUrn("Behaviors", "worker"), BehaviorTree.class).get());
         editor.createNode(nodeFactory.getNodeComponent(builder.fromJson("move_to")));
     }
 }
