1a6af88f66305234ed5ca602b89dbff1e511d133
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-03-08 17:58:06 -0800

    chore: update engine package paths for https://github.com/MovingBlocks/Terasology/pull/4560

--- src/main/java/org/terasology/behaviors/actions/DamageFollowedEntityAction.java
+++ src/main/java/org/terasology/behaviors/actions/DamageFollowedEntityAction.java
@@ -12,10 +12,10 @@ import org.terasology.engine.logic.behavior.core.Actor;
 import org.terasology.engine.logic.behavior.core.BaseAction;
 import org.terasology.engine.logic.behavior.core.BehaviorState;
 import org.terasology.engine.logic.health.EngineDamageTypes;
+import org.terasology.engine.logic.health.HealthComponent;
+import org.terasology.engine.logic.health.event.DoDamageEvent;
 import org.terasology.engine.logic.location.LocationComponent;
 import org.terasology.engine.registry.In;
-import org.terasology.logic.health.HealthComponent;
-import org.terasology.logic.health.event.DoDamageEvent;
 import org.terasology.nui.properties.Range;
 
 @BehaviorAction(name = "damage_followed_entity")
