c634bc16d41ec54276595bd7004731f2bed3783a
Tobias Nett <skaldarnar@googlemail.com>
2022-01-13 19:07:20 +0100

    fixup!: make module compile again

--- src/main/java/org/terasology/flexiblemovement/debug/DebugMoveToSystem.java
+++ src/main/java/org/terasology/flexiblemovement/debug/DebugMoveToSystem.java
@@ -1,21 +1,21 @@
-// Copyright 2021 The Terasology Foundation
+// Copyright 2022 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.flexiblemovement.debug;
 
 import org.terasology.engine.entitySystem.entity.EntityManager;
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
-import org.terasology.engine.world.block.Blocks;
-import org.terasology.flexiblemovement.FlexibleMovementComponent;
 import org.terasology.engine.logic.behavior.BehaviorComponent;
 import org.terasology.engine.logic.behavior.asset.BehaviorTree;
 import org.terasology.engine.logic.common.ActivateEvent;
-import org.terasology.gestalt.assets.management.AssetManager;
 import org.terasology.engine.registry.In;
 import org.terasology.engine.registry.Share;
+import org.terasology.engine.world.block.Blocks;
+import org.terasology.flexiblemovement.FlexibleMovementComponent;
+import org.terasology.gestalt.assets.management.AssetManager;
+import org.terasology.gestalt.entitysystem.event.ReceiveEvent;
 
 
 @Share(DebugMoveToSystem.class)
--- src/main/java/org/terasology/flexiblemovement/system/FlexibleMovementSystem.java
+++ src/main/java/org/terasology/flexiblemovement/system/FlexibleMovementSystem.java
@@ -1,4 +1,4 @@
-// Copyright 2021 The Terasology Foundation
+// Copyright 2022 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.flexiblemovement.system;
 
@@ -6,7 +6,6 @@ import com.google.common.collect.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
@@ -15,6 +14,7 @@ import org.terasology.engine.logic.characters.CharacterMoveInputEvent;
 import org.terasology.engine.logic.characters.events.HorizontalCollisionEvent;
 import org.terasology.engine.registry.Share;
 import org.terasology.flexiblemovement.FlexibleMovementComponent;
+import org.terasology.gestalt.entitysystem.event.ReceiveEvent;
 
 import java.util.Map;
 
