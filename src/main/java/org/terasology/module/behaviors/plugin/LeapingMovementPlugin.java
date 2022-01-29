5e65041d56f2e6faeb5cd034f22210c5d1ced023
Tobias Nett <skaldarnar@googlemail.com>
2022-01-29 12:22:49 +0100

    fix: use FLYING movement mode in Walking and Leaping
    The pathfinding plugins assume that entities are not affected by gravity. To match this assumption, we chose a movement mode that neglects gravity, but works similar to the regular walking mode: FLYING.

--- src/main/java/org/terasology/module/behaviors/plugin/LeapingMovementPlugin.java
+++ src/main/java/org/terasology/module/behaviors/plugin/LeapingMovementPlugin.java
@@ -1,4 +1,4 @@
-// Copyright 2021 The Terasology Foundation
+// Copyright 2022 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.module.behaviors.plugin;
 
@@ -8,6 +8,8 @@ import org.terasology.engine.core.Time;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.logic.characters.CharacterMoveInputEvent;
 import org.terasology.engine.logic.characters.CharacterMovementComponent;
+import org.terasology.engine.logic.characters.MovementMode;
+import org.terasology.engine.logic.characters.events.SetMovementModeEvent;
 import org.terasology.engine.world.WorldProvider;
 import org.terasology.flexiblepathfinding.plugins.JPSPlugin;
 import org.terasology.flexiblepathfinding.plugins.basic.LeapingPlugin;
@@ -29,11 +31,17 @@ public class LeapingMovementPlugin extends MovementPlugin {
 
     @Override
     public CharacterMoveInputEvent move(EntityRef entity, Vector3fc dest, int sequence) {
+        CharacterMovementComponent movement = entity.getComponent(CharacterMovementComponent.class);
+        // The underlying WalkingPlugin assumes that entities are not affected by gravity.
+        // To simulate this, we'll use the FLYING movement mode for all entities when moving them with this plugin.
+        if (movement.mode != MovementMode.FLYING) {
+            entity.send(new SetMovementModeEvent(MovementMode.FLYING));
+        }
+
         Vector3f delta = getDelta(entity, dest);
         float yaw = getYaw(delta);
         long dt = getTime().getGameDeltaInMs();
 
-        CharacterMovementComponent movement = entity.getComponent(CharacterMovementComponent.class);
         return new CharacterMoveInputEvent(sequence, 0, yaw, delta, false, false, true, dt);
     }
 }
--- src/main/java/org/terasology/module/behaviors/plugin/WalkingMovementPlugin.java
+++ src/main/java/org/terasology/module/behaviors/plugin/WalkingMovementPlugin.java
@@ -8,6 +8,8 @@ import org.terasology.engine.core.Time;
 import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.logic.characters.CharacterMoveInputEvent;
 import org.terasology.engine.logic.characters.CharacterMovementComponent;
+import org.terasology.engine.logic.characters.MovementMode;
+import org.terasology.engine.logic.characters.events.SetMovementModeEvent;
 import org.terasology.engine.world.WorldProvider;
 import org.terasology.flexiblepathfinding.plugins.JPSPlugin;
 import org.terasology.flexiblepathfinding.plugins.basic.WalkingPlugin;
@@ -29,11 +31,17 @@ public class WalkingMovementPlugin extends MovementPlugin {
 
     @Override
     public CharacterMoveInputEvent move(EntityRef entity, Vector3fc dest, int sequence) {
+        CharacterMovementComponent movement = entity.getComponent(CharacterMovementComponent.class);
+        // The underlying WalkingPlugin assumes that entities are not affected by gravity.
+        // To simulate this, we'll use the FLYING movement mode for all entities when moving them with this plugin.
+        if (movement.mode != MovementMode.FLYING) {
+            entity.send(new SetMovementModeEvent(MovementMode.FLYING));
+        }
+
         Vector3f delta = getDelta(entity, dest);
         float yaw = getYaw(delta);
         long dt = getTime().getGameDeltaInMs();
 
-        CharacterMovementComponent movement = entity.getComponent(CharacterMovementComponent.class);
         return new CharacterMoveInputEvent(sequence, 0, yaw, delta, false, false, false, dt);
     }
 }
