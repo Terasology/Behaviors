458867e56df791f973e6804b84941a41bffb2bfd
Josephine Rueckert <jd.rueckert@googlemail.com>
2023-10-22 16:04:32 +0200

    qa: remove unused imports

--- src/main/java/org/terasology/module/behaviors/actions/SetTargetToNearbyPlayer.java
+++ src/main/java/org/terasology/module/behaviors/actions/SetTargetToNearbyPlayer.java
@@ -2,17 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.module.behaviors.actions;
 
-import org.terasology.engine.registry.CoreRegistry;
-import org.terasology.module.behaviors.components.AttackOnHitComponent;
-import org.terasology.module.behaviors.components.FindNearbyPlayersComponent;
-import org.terasology.module.behaviors.components.FollowComponent;
 import org.terasology.engine.core.Time;
 import org.terasology.engine.logic.behavior.BehaviorAction;
 import org.terasology.engine.logic.behavior.core.Actor;
 import org.terasology.engine.logic.behavior.core.BaseAction;
 import org.terasology.engine.logic.behavior.core.BehaviorState;
+import org.terasology.engine.registry.CoreRegistry;
 import org.terasology.engine.registry.In;
-import org.terasology.module.behaviors.systems.PluginSystem;
+import org.terasology.module.behaviors.components.AttackOnHitComponent;
+import org.terasology.module.behaviors.components.FindNearbyPlayersComponent;
+import org.terasology.module.behaviors.components.FollowComponent;
 
 @BehaviorAction(name = "set_target_nearby_player")
 public class SetTargetToNearbyPlayer extends BaseAction {
