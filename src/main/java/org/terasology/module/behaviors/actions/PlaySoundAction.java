3f069bc9cce1f21645c64476d42b630397f3e6ac
BenjaminAmos <24301287+BenjaminAmos@users.noreply.github.com>
2024-08-04 15:40:52 +0100

    fix: fix compilation with Gestalt 8 (#117)
    * requires at least Inventory v1.5.1
    
    ---------
    
    Co-authored-by: jdrueckert <jd.rueckert@googlemail.com>

--- src/main/java/org/terasology/module/behaviors/actions/PlaySoundAction.java
+++ src/main/java/org/terasology/module/behaviors/actions/PlaySoundAction.java
@@ -15,7 +15,7 @@ import org.terasology.engine.logic.behavior.core.BaseAction;
 import org.terasology.engine.logic.behavior.core.BehaviorState;
 import org.terasology.engine.logic.location.LocationComponent;
 import org.terasology.engine.registry.In;
-import org.terasology.gestalt.module.sandbox.API;
+import org.terasology.context.annotation.API;
 import org.terasology.nui.properties.OneOf;
 import org.terasology.nui.properties.Range;
 
